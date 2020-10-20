ef728f1ac409c966e02d12d0d4752de15f900c8b
BonDah18 <proctorine@gmail.com>
2020-10-19 17:35:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-job-listing
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-job-listing)
---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import {Switch, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import "./style.css";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/jobs" component={Jobs} />
+          <Route path="/candidates" component={Candidates} />
+          <Route path="/job_details" component={JobDetails} />
+          <Route path="/elements" componenet={Elements} />
+          <Route path="/blog" components={Blog} />
+          <Route path="/single-blog" components={SingleBlog} />
+          <Route path="contact" components={Contact} />
+        </Switch>
+      </React.Fragment>
+
+    );
+  }
+}
+  
+
+
+export default App
+
+
+
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Job Seekers!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}

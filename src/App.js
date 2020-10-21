b98c2aefdd9836a8c6ee35f5f7aaf06986f76bdf
BonDah18 <proctorine@gmail.com>
2020-10-20 23:34:07 -0700

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import {Switch, Route} from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './Components/Navbar';
 import "./style.css";
 
 class App extends Component {

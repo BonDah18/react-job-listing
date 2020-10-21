978a1aca87028e497dcfbdcda91cdc29cddb9421
BonDah18 <proctorine@gmail.com>
2020-10-20 23:38:12 -0700

    second commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
-import logo from '../logo.svg';
-import { ButtonContainer } from './Button';
+
 
 
 export default class Navbar extends Component {
